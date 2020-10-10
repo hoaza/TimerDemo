a2413b51b03bbecc2c90d9d2bb6e366c6a2e46b6
Haizinger Lukas <L.Haizinger@scheuch.com>
2020-10-10 08:41:22 +0200

    Add project files.

---
+++ TimerDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Start();
+        }
+    }
+}
---
+++ TimerDemo/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace TimerDemo
+{
+    public class Timer
+    {
+        private Thread ticker;
+
+        public Timer()
+        {
+            this.ticker = new Thread(OnTick);
+        }
+
+        public void Start() => this.ticker.Start();
+
+        private void OnTick()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Timer elapsed!");
+            }
+        }
+    }
+}
